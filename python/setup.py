1c1bf4fd4475643cd20d9599aab12f674f57b925
nazar <borodavkanazar@gmail.com>
2026-05-30 18:36:16 +0200

    test

---
+++ python/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+# Keep this file as a no-op placeholder so any accidental invocation is harmless.
+setup(name="placeholder_extension", version="0.0.0", py_modules=[])
